8dacfeab0e291bab184e9faa184e38aeb724c279
McWojownik <49338133+McWojownik@users.noreply.github.com>
2020-08-25 21:36:05 +0200

    FINALLY FINISHED

--- static/js/other_js/Settings.js
+++ static/js/other_js/Settings.js
@@ -1,6 +1,6 @@
 const settings = {
-    // url: "https://mc-mp3-player.herokuapp.com",
-    url: "http://localhost:3000",
+    url: "https://mc-mp3-player.herokuapp.com",
+    // url: "http://localhost:3000",
     mongoBegin: "mongodb+srv://McWojownik:DMmNHHfVSff3yV3t@cluster0.wz5ya.mongodb.net/",
     mongoEnd: "?retryWrites=true&w=majority"
 }
