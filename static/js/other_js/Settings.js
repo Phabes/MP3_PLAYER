bbc0bdf5dbcdd2098f355b877f35217239524d8c
McWojownik <49338133+McWojownik@users.noreply.github.com>
2022-03-17 01:47:56 +0100

    connection updated

--- static/js/other_js/Settings.js
+++ static/js/other_js/Settings.js
@@ -1,4 +1,4 @@
 const settings = {
-  url: "http://192.168.0.107:3000",
-  //   url: "https://mc-mp3-player.herokuapp.com",
+  // url: "http://192.168.0.107:3000",
+  url: "https://mc-mp3-player.herokuapp.com",
 };
